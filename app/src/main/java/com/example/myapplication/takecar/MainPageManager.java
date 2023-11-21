6cdaa97a70a6edbf4e9eb983d8b226bfc278e318
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2023-11-21 19:24:14 +0100

    hide app on double back press

--- app/src/main/java/com/example/myapplication/takecar/MainPageManager.java
+++ app/src/main/java/com/example/myapplication/takecar/MainPageManager.java
@@ -11,11 +11,23 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainPageManager extends AppCompatActivity {
 
+    private static final int CLOSE_APP_INTERVAL = 1000;
+    private long backPressedTime = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + CLOSE_APP_INTERVAL > System.currentTimeMillis()) {
+            moveTaskToBack(true);
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
+
 
     public void logout(View view) {
         FirebaseAuth mAuth;
