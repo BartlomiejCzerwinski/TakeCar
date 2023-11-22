a2cf79fa29fea911fc7be205af4355a33058b7ac
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2023-11-22 15:24:39 +0100

    add custom switch on main page

--- app/src/main/java/com/example/myapplication/takecar/MainPageManager.java
+++ app/src/main/java/com/example/myapplication/takecar/MainPageManager.java
@@ -1,11 +1,15 @@
 package com.example.myapplication.takecar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,10 +18,18 @@ public class MainPageManager extends AppCompatActivity {
     private static final int CLOSE_APP_INTERVAL = 1000;
     private long backPressedTime = 0;
 
+    private SwitchCompat switchCompat;
+    private TextView tvTake;
+    private TextView tvRent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        switchCompat = findViewById(R.id.switchCompat);
+        tvTake = findViewById(R.id.tvTake);
+        tvRent = findViewById(R.id.tvRent);
+        setSwitchListener();
     }
 
     @Override
@@ -40,5 +52,32 @@ public class MainPageManager extends AppCompatActivity {
     public void openAddCarForm(View view) {
         Intent intent = new Intent(this, AddCarManager.class);
         startActivity(intent);
+    }
+
+    public void setSwitchListener() {
+        int colorWhite = Color.parseColor("#E2E2E2");
+        int colorBlack = Color.parseColor("#2E2E2E");
+        boolean isSwitchChecked = switchCompat.isChecked();
+
+        if (isSwitchChecked) {
+            tvTake.setTextColor(colorBlack);
+            tvRent.setTextColor(colorWhite);
+        } else {
+            tvTake.setTextColor(colorWhite);
+            tvRent.setTextColor(colorBlack);
+        }
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    tvTake.setTextColor(colorBlack);
+                    tvRent.setTextColor(colorWhite);
+                } else {
+                    tvTake.setTextColor(colorWhite);
+                    tvRent.setTextColor(colorBlack);
+                }
+            }
+        });
     }
 }
