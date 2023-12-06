e3974c2aee8802c6ba8bcd93348d4485d7699a38
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2023-12-06 18:28:28 +0100

    car page update

--- app/src/main/java/com/example/myapplication/takecar/CarPageManager.java
+++ app/src/main/java/com/example/myapplication/takecar/CarPageManager.java
@@ -11,12 +11,23 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CarPageManager extends AppCompatActivity {
 
     private Car car;
+
+    private TextView tvYear;
+    private TextView tvTitle;
+    private TextView tvPlaces;
+    private TextView tvDoors;
+    private TextView tvGearbox;
+    private TextView tvAirConditioner;
+    private TextView tvPower;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_page);
+        setTextViewObjects();
         car = getCarFromIntent();
         System.out.println("CAR PAGE DATA:" + car.toString());
+        initCarPage();
     }
 
     public Car getCarFromIntent() {
@@ -26,6 +37,30 @@ public class CarPageManager extends AppCompatActivity {
             car = (Car) intent.getSerializableExtra("car");
         }
         return car;
+    }
+
+    public void setTextViewObjects() {
+        tvYear = findViewById(R.id.tvYearCarPage);
+        tvTitle = findViewById(R.id.tvTitleCarPage);
+        tvPlaces = findViewById(R.id.tvPlacesCarPage);
+        tvDoors = findViewById(R.id.tvDoorsCarPage);
+        tvGearbox = findViewById(R.id.tvGearboxCarPage);
+        tvAirConditioner = findViewById(R.id.tvAcCarPage);
+        tvPower = findViewById(R.id.tvPowerCarPage);
+    }
+
+    public void initCarPage() {
+        setTextView(tvYear, Integer.toString(car.getYear()));
+        setTextView(tvTitle, car.getProducer() + " " + car.getModel());
+        setTextView(tvPlaces, Integer.toString(car.getPlaces()));
+        setTextView(tvDoors, Integer.toString(car.getDoors()));
+        setTextView(tvGearbox, car.getGearbox());
+        setTextView(tvAirConditioner, car.isAirConditioner());
+        setTextView(tvPower, Integer.toString(car.getPower()));
+    }
+
+    public void setTextView(TextView textView, String text) {
+        textView.setText(text);
     }
 
 }
