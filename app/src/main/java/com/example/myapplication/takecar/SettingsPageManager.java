0db667daba0500d8781d5936c6a99a2512b986d3
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-01-04 20:44:10 +0100

    update settings page

--- app/src/main/java/com/example/myapplication/takecar/SettingsPageManager.java
+++ app/src/main/java/com/example/myapplication/takecar/SettingsPageManager.java
@@ -1,9 +1,13 @@
 package com.example.myapplication.takecar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsPageManager extends AppCompatActivity {
 
@@ -11,5 +15,13 @@ public class SettingsPageManager extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
